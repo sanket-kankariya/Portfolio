fca66ff3ddf18f575a3c6e9c62da64ad5cf7293c
Sanket Kankariya <sanketkankariya.dev@gmail.com>
2024-01-27 11:09:41 +0530

    header added

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -11,30 +13,50 @@ class HomeView extends GetView<HomeController> {
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
+        toolbarHeight: 80,
         backgroundColor: Colors.transparent,
-        title: const Text(
-          'Sanket Kankariya',
-          style: TextStyle(color: Colors.white),
-        ),
         actions: [
           Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ElevatedButton.icon(
-              icon: const Icon(
-                Icons.portrait,
-                color: Colors.white,
-              ),
-              style: const ButtonStyle(
+            padding: const EdgeInsets.only(right: 30.0),
+            child: ElevatedButton(
+              style: ButtonStyle(
                   shape: MaterialStatePropertyAll(RoundedRectangleBorder(
                       borderRadius: BorderRadius.all(Radius.circular(5)))),
-                  backgroundColor: MaterialStatePropertyAll(Colors.green)),
+                  backgroundColor: MaterialStatePropertyAll(Colors.green[400])),
               onPressed: () {},
-              label: const Text(
-                'Contact Me',
-                style: TextStyle(color: Colors.white),
+              child: Wrap(
+                direction: Axis.horizontal,
+                children: [
+                  const Text(
+                    'Contact Me',
+                    style: TextStyle(color: Color.fromARGB(255, 223, 223, 223)),
+                  ),
+                ],
               ),
             ),
           ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Container(
+            height: 200,
+            width: Get.width,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage(
+                  'https://images.unsplash.com/photo-1614850715973-58c3167b30a0?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+                ),
+                fit: BoxFit.cover,
+              ),
+              
+            ),
+            child: Row(
+              children: [
+                CircleAvatar(),
+              ],
+            ),
+          )
         ],
       ),
     );
